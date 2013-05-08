6c6ebaa68d0fb94b006b50d59ac7f729cfefea47
hayksaakian <fakest.id.ever@gmail.com>
2013-05-08 12:35:52 -0700

    ok

--- bench.rb
+++ bench.rb
@@ -6,14 +6,14 @@ Benchmark.bm do|b|
   b.report("GET localhost:4567/random_domain ") do
     a = ""
     domains = ["http://google.com", "google.com", "amazon.com", "http://amazon.com", "apple.com", "http://apple.com"]
-    1_000.times { 
+    100.times { 
     	Net::HTTP::get(URI.parse('http://localhost:4567/'<<domains.sample)) 
     }
   end
   b.report("GET domain2ip.net/random_domain ") do
     a = ""
     domains = ["http://google.com", "google.com", "amazon.com", "http://amazon.com", "apple.com", "http://apple.com"]
-    1_000.times { 
+    100.times { 
     	Net::HTTP::get(URI.parse('http://domain2ip.net/'<<domains.sample)) 
     }
   end
